22b171ecbb75ba7d10cf5c1c457fd096dc4b4ab8
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-10-25 18:45:09 -0400

    Create data seeding

--- simulator/sensor_simulator.py
+++ simulator/sensor_simulator.py
@@ -13,19 +13,40 @@ import json
 import time
 import math
 import random
+import config
 
 def simulate_sensors():
     """
     Generate realistic sensor readings for a tomato greenhouse
     Returns: dict with sensor values
     """
-    pass
+    sensor_values = {
+    "temperature": config.BASE_TEMP,
+    "humidity": config.BASE_HUMIDITY,
+    "soil_moisture": config.BASE_SOIL,
+    "light_level": config.BASE_LIGHT,
+    "co2_ppm": config.BASE_CO2
+    }
+
+    return sensor_values
 
 def main():
     """
     Main loop: generate sensor data and publish to Redis
     """
-    pass
+    redis_client = redis.Redis(host = config.REDIS_HOST, port = config.REDIS_PORT)
+
+
+    # Define the channel and the message
+    channel_name = config.REDIS_CHANNEL
+    sensor_values = simulate_sensors()
+    while(1):
+        # num_subscribers = redis_client.publish(channel_name, sensor_values)
+
+        print(f"Message '{sensor_values}' published to channel '{channel_name}'.\n", flush=True)
+        # print(f"Number of subscribers that received the message: {num_subscribers}\n")
+
+        time.sleep(config.UPDATE_INTERVAL)
 
 if __name__ == "__main__":
     main()
