6c8f5d0a06e577d667504b2a73137ebca31d4a98
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-10-26 02:49:14 -0400

    Initialize websocket

--- frontend/src/hooks/useWebSocket.js
+++ frontend/src/hooks/useWebSocket.js
@@ -2,7 +2,7 @@
 // Purpose: Custom React hook for real-time data connection
 //
 // Usage:
-// const { data, connected, error } = useWebSocket('http://localhost:3001');
+const { data, connected, error } = useWebSocket('http://localhost:3001');
 
 import { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
@@ -14,20 +14,20 @@ export function useWebSocket(serverUrl) {
 
   useEffect(() => {
     // TODO: Initialize socket connection
-    // const socket = io(serverUrl);
+    const socket = io(serverUrl);
 
     // TODO: Listen for sensor-update events
-    // socket.on('sensor-update', (newData) => {
-    //   setData(newData);
-    // });
+    socket.on('sensor-update', (newData) => {
+      setData(newData);
+    });
 
     // TODO: Handle connection events
-    // socket.on('connect', () => setConnected(true));
-    // socket.on('disconnect', () => setConnected(false));
-    // socket.on('error', (err) => setError(err));
+    socket.on('connect', () => setConnected(true));
+    socket.on('disconnect', () => setConnected(false));
+    socket.on('error', (err) => setError(err));
 
     // TODO: Cleanup on unmount
-    // return () => socket.disconnect();
+    return () => socket.disconnect();
   }, [serverUrl]);
 
   return { data, connected, error };
